fa1f7a0864fb3566807d44557a1b22eedbeaa1ce
anton <asugolov@gmail.com>
2026-02-15 23:16:23 -0500

    blog

--- blog/build.sh
+++ blog/build.sh
@@ -3,7 +3,7 @@
 # Step 1: call Pandoc to convert to Markdown
 for f in md/*.md; do
   date=$(head -n 6 $f | grep date | sed "s/\-//g" | grep -o "[0-9]\+")
-  pandoc "$f" -s --css=../../../styles.css --css=../../blog.css --toc -N --toc-depth 4 -B ../template/nav.html --mathjax -o "posts/$date/index.html"
+  pandoc "$f" -s --css=../../../styles.css --css=../../blog.css --toc -N --toc-depth 4 -H template/meta_jepax.html -B ../template/nav.html --mathjax -o "posts/$date/index.html"
 done
 
 # Step 2: built the list of posts
